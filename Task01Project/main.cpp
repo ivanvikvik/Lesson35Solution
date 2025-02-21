5912d6d1d4602df822a055f3b933dff864154b54
ivanvikvik <ivanvikvik@gmail.com>
2025-02-21 17:59:16 +0300

    Add project files.

---
+++ Task01Project/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "logic.h"
+using namespace std;
+
+int main() {
+	int mark1, mark2, mark3, mark4, mark5;
+
+	cout << "Input students' marks: ";
+	cin >> mark1 >> mark2 >> mark3 >> mark4 >> mark5;
+
+	double avg = calculate_avg_mark(mark1, mark2, mark3, mark4, mark5);
+
+	cout << "Students average mark is " << avg << ".\n";
+
+	return 0;
+}

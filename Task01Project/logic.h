b6086d12a3b1b98407d1ecc1e59d36c9f13a726e
ivanvikvik <ivanvikvik@gmail.com>
2025-02-21 18:06:45 +0300

    Update project

--- Task01Project/logic.h
+++ Task01Project/logic.h
@@ -1,1 +1,21 @@
 double calculate_avg_mark(int mark1, int mark2, int mark3, int mark4, int mark5);
+
+//Lesson35Solution
+//	- Task01Project
+//	- Task02Project
+//	- ...
+//
+//Homework35BasicSolution
+//	- Task01Project
+//	- Task02Project
+//	- ...
+//
+//Homework35MainSolution
+//	- Task01Project
+//	- Task02Project
+//	- ...
+//
+//Homework35AdvanceSolution	<--> Homework35AdvanceProject
+//	- Task01Project
+//	- Task02Project
+//	- ...
